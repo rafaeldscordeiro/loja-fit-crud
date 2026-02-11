ace66f351f9bea6aa0b3d03e7eaf207b0b079905
rafaeldscordeiro <rafael.dscordeiro@gmail.com>
2026-02-11 16:54:21 -0300

    feat: rota inicial de produtos

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const produtosRoutes = require("./routes/produtos.routes");
+app.use("/produtos", produtosRoutes);
+
 app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
